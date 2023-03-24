f89af87830115d605ad2a21f14e80270d45263ea
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-24 15:22:30 +0100

    add posts from js

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,12 +70,25 @@ const posts = [
     }
 ];
 
-
 // Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
 // Take the DOM container 
 // loop into "post" with foreach
     // Insert each post with insertadjacentHTMl and templete literal 
+
+const containerEl = document.getElementById("container"); 
+
+posts.forEach((post) => {
+    
+    containerEl.insertAdjacentHTML("beforeend", addPost(post))
+})
+
+
+
+
+
+
+
 
 
 
@@ -86,3 +99,41 @@ const posts = [
 // Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
 // Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 
+
+
+
+
+// function to add posts
+function addPost (post) {
+    const postMarkup = `<div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`;
+        return postMarkup
+}
